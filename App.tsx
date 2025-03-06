23f7e8b58c472cc947895e79a5464d11d6f88fcd
Marcelo Costa <marcelodscosta@yahoo.com.br>
2025-03-06 12:23:36 -0300

    [chore] Font loading

--- App.tsx
+++ App.tsx
@@ -2,10 +2,17 @@ import { View, Text } from 'react-native';
 
 import './global.css';
 
+import { Poppins_400Regular, Poppins_700Bold, useFonts } from '@expo-google-fonts/poppins';
+
 export default function App() {
+  const [fontLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_700Bold,
+  });
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View className="flex-1 items-center justify-center bg-slate-600">
+      {fontLoaded ? <Text className="text-white">Home</Text> : <View />}
     </View>
   );
 }
