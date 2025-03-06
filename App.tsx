5c011e2c69d712abdbb5a420a2cf479524b3fbbc
Marcelo Costa <marcelodscosta@yahoo.com.br>
2025-03-06 12:35:09 -0300

    [feat] Loading component

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { View, Text } from 'react-native';
 import './global.css';
 
 import { Poppins_400Regular, Poppins_700Bold, useFonts } from '@expo-google-fonts/poppins';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontLoaded] = useFonts({
@@ -12,7 +13,7 @@ export default function App() {
 
   return (
     <View className="flex-1 items-center justify-center bg-slate-600">
-      {fontLoaded ? <Text className="text-white">Home</Text> : <View />}
+      {!fontLoaded ? <Text className="text-white">Home</Text> : <Loading />}
     </View>
   );
 }
