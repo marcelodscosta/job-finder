bfdae6896c227c8174b2e2c5eaf6ba2cfce0cbad
Marcelo Costa <marcelodscosta@yahoo.com.br>
2025-03-15 21:57:37 -0300

    [feat] Create navigation context

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import { Loading } from '@components/Loading';
 import { Poppins_400Regular, Poppins_700Bold, useFonts } from '@expo-google-fonts/poppins';
+import { Routes } from '@routes/index';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import './global.css';
-import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontLoaded] = useFonts({
@@ -14,7 +14,7 @@ export default function App() {
   return (
     <View className="flex-1">
       <StatusBar style="auto" translucent />
-      {fontLoaded ? <SignIn /> : <Loading />}
+      {fontLoaded ? <Routes /> : <Loading />}
     </View>
   );
 }
